c9b0463026cd15e1ccc6ef6e1e104cf09430832f
Leofront-end <ls171258@gmail.com>
2025-09-29 19:15:21 -0300

    feat: Impactube.py

--- ImpacTube.py
+++ ImpacTube.py
@@ -6,17 +6,15 @@ for i in range(0,N):
     monetizacao = float(monetizacao)
     cadastro.append([nome,inscritos,monetizacao,premium])
 
-X = float(input(''))
-Y = float(input(''))
+ehPremium = float(input(''))
+naoPremium = float(input(''))
 print('-----')
 print('BÔNUS')
 print('-----')
 for nome,inscritos,monetizacao,premium in cadastro:
     valorMais = 0
     if (premium == 'sim'):
-        valorMais = (inscritos//1000) * X + monetizacao
+        valorMais = (inscritos//1000) * ehPremium + monetizacao
     else:
-        valorMais = (inscritos//1000) * Y + monetizacao
+        valorMais = (inscritos//1000) * naoPremium + monetizacao
     print(f'{nome}: R$ {valorMais:.2f}')
-
-    
